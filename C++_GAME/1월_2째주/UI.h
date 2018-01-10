41c9c1f102169abccee7ebe0a3ee3187bf15993d
hs <astaricia@gmail.com>
2018-01-10 17:50:53 +0900

    . 추가

--- C++_GAME/1월_2째주/UI.h
+++ C++_GAME/1월_2째주/UI.h
@@ -9,13 +9,9 @@ public:
 	// sentence 출력, 배열의 크기는 num에 저장
 	// 함수 시작 시 콘솔 창 clear
 	static void print(string sentence[], int num);
-	// sentence 출력, 문장이 한개일 경우 사용
-	// 함수 시작 시 콘솔 창 clear
-	static void print(string sentence);
 
 	// 사용자로부터 string 값을 받아와 반환, 출력되는 것은 없음
 	// 주로 이름설정 등에 사용됨
 	// 입력가능한 문자열의 길이 제한해야함
-	static void setValue(string& newString);
-	static void setValue(bool& newBool);
+	static void setValue(string& newString); // .
 };
