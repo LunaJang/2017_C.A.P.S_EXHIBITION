c3b67eaacb21a4a73eca5e53e6d2990b721f7703
hs <astaricia@gmail.com>
2018-03-06 19:18:29 +0900

    헤더 교체

--- C++_GAME/header/UI.h
+++ C++_GAME/header/UI.h
@@ -4,14 +4,16 @@
 class UI {
 public:
 	// option 출력, 배열의 크기는 num에 저장
-	static int printOption(string option[], int num);
+	static int printOption(vector<string>&option, int num);
 
 	// sentence 출력, 배열의 크기는 num에 저장
 	// 함수 시작 시 콘솔 창 clear
-	static void print(string sentence[], int num);
+	static void print(vector <string>&sentence);
 
 	// 사용자로부터 string 값을 받아와 반환, 출력되는 것은 없음
 	// 주로 이름설정 등에 사용됨
 	// 입력가능한 문자열의 길이 제한해야함
 	static void setValue(string& newString); // 
+
+	static int printScript(vector<string>&scriptbox);
 };
